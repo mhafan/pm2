16949771d2ecd8cdb8af63bf61c055cd874e60ae
Martin Hrubý <hrubym@fit.vutbr.cz>
2020-03-21 09:07:26 +0100

    d

--- Package.swift
+++ Package.swift
@@ -6,15 +6,8 @@ import PackageDescription
 let package = Package(
     name: "pm2",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "pm2",
-            targets: ["pm2"]),
-        
-        .library(
-            name: "cals",
-            targets: ["cals"]),
-        
+        .library(name: "pm2", targets: ["pm2"]),
+        .library(name: "cals", targets: ["cals"]),
         .library(name: "numas", targets: ["numas"])
     ],
     dependencies: [
@@ -24,9 +17,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "pm2",
-            dependencies: []),
+        .target(name: "pm2", dependencies: ["cals", "numas"]),
         .target(
             name: "cals",
             dependencies: []),
